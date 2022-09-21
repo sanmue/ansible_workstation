8c5512d49fd5e92f01ae5b2eceaa29fbcf3e0f15
sanmue <sanmue@nomail.com>
2022-09-21 17:40:30 +0200

    Formatierungen im skript

--- tasks/bootstrap_ubuntu.sh
+++ tasks/bootstrap_ubuntu.sh
@@ -16,15 +16,19 @@ userid=$(whoami)
 #    exit 0
 #fi
 
+echo ""
 echo "Update Repos und Installation benoetigte Software ..."
 sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get install -y --show-progress git ansible chrome-gnome-shell ssh ufw
 
+echo ""
 echo "Installiere benötigte Packages für Installation von Microsoft PowerShell"
 sudo apt-get install -y --show-progress wget apt-transport-https software-properties-common
 
+echo ""
 echo "Installiere noch fehlende, benötigte Packages für Installation von Brave Web Browser"
 sudo apt-get install -y --show-progress curl
 
+echo ""
 echo "Aktiviere Firewall 'ufw' und erlaube ssh ..."
 sudo ufw enable && sudo ufw allow ssh comment 'SSH' && sudo ufw reload
 
