c51f4553ac7743fffa4fa24850e870dc0eab7cea
sanmue <sanmue@nomail.com>
2022-09-22 22:34:31 +0200

    Änderungen Skript terminal Preferences

--- tasks/config_workstation-desktopPreferences-terminal.sh
+++ tasks/config_workstation-desktopPreferences-terminal.sh
@@ -3,21 +3,20 @@
 # based on: https://askubuntu.com/questions/270469/how-can-i-create-a-new-profile-for-gnome-terminal-via-command-line
 # (gsettings: https://ncona.com/2019/11/configuring-gnome-terminal-programmatically/)
 
-dconfdir=/org/gnome/terminal/legacy/profiles:
+dconfdir="/org/gnome/terminal/legacy/profiles:"
 
 create_new_profile() {
-    local profile_ids=($(dconf list $dconfdir/ | grep ^: |\
-                        sed 's/\///g' | sed 's/://g'))
+    local profile_ids=("$(dconf list ${dconfdir}/ | grep ^: | sed 's/\///g' | sed 's/://g')")
     local profile_name="$1"
-    local profile_ids_old="$(dconf read "$dconfdir"/list | tr -d "]")"
+    local profile_ids_old="$(dconf read "${dconfdir}"/list | tr -d "]")"
     local profile_id="$(uuidgen)"
 
-    [ -z "$profile_ids_old" ] && local profile_ids_old="["  # if there's no `list` key
+    [ -z "${profile_ids_old}" ] && local profile_ids_old="["  # if there's no `list` key
     [ ${#profile_ids[@]} -gt 0 ] && local delimiter=,  # if the list is empty
     dconf write $dconfdir/list \
-        "${profile_ids_old}${delimiter} '$profile_id']"
-    dconf write "$dconfdir/:$profile_id"/visible-name "'$profile_name'"
-    echo $profile_id
+        "${profile_ids_old}${delimiter} '${profile_id}']"
+    dconf write "${dconfdir}/:${profile_id}"/visible-name "'${profile_name}'"
+    echo "${profile_id}"
 }
 
 
