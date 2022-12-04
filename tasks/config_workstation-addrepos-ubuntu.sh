3e23012705d7adc4772b8491ff8a50970b487451
Sandro <sandro@universalaccount.de>
2022-12-04 23:02:32 +0100

    config_workstation-addrepos-ubuntu.sh: Update - Quellenangabe

--- tasks/config_workstation-addrepos-ubuntu.sh
+++ tasks/config_workstation-addrepos-ubuntu.sh
@@ -20,12 +20,12 @@ done
 
 
 # Set PPA Priority for Mozillateam and block Firefox from Ubuntu’s own repository
+# Quelle: https://fostips.com/ubuntu-21-10-two-firefox-remove-snap/#rb-Step-2-Install-back-the-classic-Firefox-Deb-package
 dir="/etc/apt/preferences.d"
 mozfile="99mozillateamppa"
 
 touch "${dir}/${mozfile}"
 
-# nächster Teil darf nicht eingerückt werden:
 cat > "${dir}/${mozfile}" << EOF
 Package: firefox*
 Pin: release o=LP-PPA-mozillateam
