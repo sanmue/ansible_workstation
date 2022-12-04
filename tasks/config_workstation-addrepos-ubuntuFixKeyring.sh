49fde3995de4d1b42ceddedd28eee7efcacf9196
Sandro <sandro@universalaccount.de>
2022-12-04 20:02:19 +0100

    Ubuntu >=22: Fix deprecation Warning bei apt-update

---
+++ tasks/config_workstation-addrepos-ubuntuFixKeyring.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fix deprecation warning bei apt-update: Key is stored in legacy trusted.gpg keyring (/etc/apt/trusted.gpg)
+#   - z.B.: http://ppa.launchpad.net/mozillateam/ppa/ubuntu/dists/jammy/InRelease: Key is stored in legacy trusted.gpg keyring (/etc/apt/trusted.gpg), see the DEPRECATION section in apt-key(8) for details.
+# Quelle: https://askubuntu.com/questions/1407632/key-is-stored-in-legacy-trusted-gpg-keyring-etc-apt-trusted-gpg
+
+for KEY in $( \
+    apt-key --keyring /etc/apt/trusted.gpg list \
+    | grep -E "(([ ]{1,2}(([0-9A-F]{4}))){10})" \
+    | tr -d " " \
+    | grep -E "([0-9A-F]){8}\b" \
+); do
+    K=${KEY:(-8)}
+    apt-key export "${K}" \
+    | sudo gpg --dearmour -o "/etc/apt/trusted.gpg.d/imported-from-trusted-gpg-${K}.gpg"
+done
+
+# cleanup:
+#rm -f /etc/apt/trusted.gpg.d/imported-from-trusted-gpg-*.gpg
