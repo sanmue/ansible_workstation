c6ac3ef531db3ea8a3018fc51bab252ff7011b9a
Sandro <sandro@universalaccount.de>
2022-12-04 17:13:22 +0100

    bootstrap

--- tasks/bootstrap_copyToTarget-TestLab.sh
+++ tasks/bootstrap_copyToTarget-TestLab.sh
@@ -44,8 +44,7 @@ echo "Kopiere bootstrap-Skript ins Home-Verzeichnis von '${userid}' auf Zielrech
 #rsync -avPEzh --stats --include="*.sh" --exclude={"bootstrap_copyToTarget.sh","config_workstation-desktopPreferences-terminal.sh","*.yml*"} "./" "${userid}@${targetIP}:~"   # kopiert nicht nur alle .sh außer den excludeten, sondern auch die excludeten mit (warum?):
 # https://unix.stackexchange.com/questions/307862/rsync-include-only-certain-files-types-excluding-some-directories
 # current:
-#rsync -avPEzh --stats --exclude={"bootstrap_copyToTarget-TestLab.sh","config_workstation-desktopPreferences-terminal.sh","config_all-servcices-misc-vim.sh","*.yml*"} --include="*.sh" "./" "${userid}@${targetIP}:~"
-rsync -PEzhv --stats --exclude={"bootstrap_copyToTarget-TestLab.sh","config_workstation-desktopPreferences-terminal.sh","config_all-servcices-misc-vim.sh","*.yml*"} --include="*.sh" "./" "${userid}@${targetIP}:~"
+rsync -avPEzh --stats --exclude={"bootstrap_copyToTarget-TestLab.sh","config_workstation-desktopPreferences-terminal.sh","config_all-servcices-misc-vim.sh","*.yml*"} --include="*.sh" "./" "${userid}@${targetIP}:~"
 
 
 # Copy login ssh-KeyFile to target
