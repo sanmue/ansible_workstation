d8071c74cb3df8ce8c475f9dbde525d71639f41a
Sandro <sandro@universalaccount.de>
2022-12-04 15:50:54 +0100

    config_workstation-desktopPreferences-terminal.sh: Update - format bash-variablen

--- tasks/config_workstation-desktopPreferences-terminal.sh
+++ tasks/config_workstation-desktopPreferences-terminal.sh
@@ -6,18 +6,18 @@
 dconfdir=/org/gnome/terminal/legacy/profiles:
 
 create_new_profile() {
-    local profile_ids=($(dconf list $dconfdir/ | grep ^: |\
-                        sed 's/\///g' | sed 's/://g'))
+    local profile_ids="($(dconf list ${dconfdir}/ | grep ^: |\
+                        sed 's/\///g' | sed 's/://g'))"
     local profile_name="$1"
-    local profile_ids_old="$(dconf read "$dconfdir"/list | tr -d "]")"
+    local profile_ids_old="$(dconf read "${dconfdir}"/list | tr -d "]")"
     local profile_id="$(uuidgen)"
 
-    [ -z "$profile_ids_old" ] && local profile_ids_old="["  # if there's no `list` key
+    [ -z "${profile_ids_old}" ] && local profile_ids_old="["  # if there's no `list` key
     [ ${#profile_ids[@]} -gt 0 ] && local delimiter=,  # if the list is empty
-    dconf write $dconfdir/list \
+    dconf write ${dconfdir}/list \
         "${profile_ids_old}${delimiter} '$profile_id']"
-    dconf write "$dconfdir/:$profile_id"/visible-name "'$profile_name'"
-    echo $profile_id
+    dconf write "${dconfdir}/:${profile_id}"/visible-name "'${profile_name}'"
+    echo "${profile_id}"
 }
 
 
@@ -25,42 +25,42 @@ create_new_profile() {
 ### Create profile - "Custom_Standard"
 id=$(create_new_profile Custom_Standard)
 # Preferences:
-dconf write "$dconfdir/:$id"/background-color "'rgb(0,43,54)'"
-dconf write "$dconfdir/:$id"/foreground-color "'rgb(131,148,150)'"
-dconf write "$dconfdir/:$id"/use-theme-colors "true"
-dconf write "$dconfdir/:$id"/default-size-columns "100"
-dconf write "$dconfdir/:$id"/default-size-rows "28"
-dconf write "$dconfdir/:$id"/font "'Monospace 14'"
-dconf write "$dconfdir/:$id"/use-system-font "false"
+dconf write "${dconfdir}/:${id}"/background-color "'rgb(0,43,54)'"
+dconf write "${dconfdir}/:${id}"/foreground-color "'rgb(131,148,150)'"
+dconf write "${dconfdir}/:${id}"/use-theme-colors "true"
+dconf write "${dconfdir}/:${id}"/default-size-columns "100"
+dconf write "${dconfdir}/:${id}"/default-size-rows "28"
+dconf write "${dconfdir}/:${id}"/font "'Monospace 14'"
+dconf write "${dconfdir}/:${id}"/use-system-font "false"
 
 # Set as default profile:
-dconf write "$dconfdir/default" "'$id'"
+dconf write "${dconfdir}/default" "'$id'"
 
 
 ######################################
 ### Create profile - "Custom_SSH"
 id=$(create_new_profile Custom_SSH)
 # Preferences:
-dconf write "$dconfdir/:$id"/background-color "'rgb(0,0,0)'"
-dconf write "$dconfdir/:$id"/foreground-color "'rgb(0,255,0)'"
-dconf write "$dconfdir/:$id"/use-theme-colors "false"
-dconf write "$dconfdir/:$id"/default-size-columns "100"
-dconf write "$dconfdir/:$id"/default-size-rows "28"
-dconf write "$dconfdir/:$id"/font "'Monospace 14'"
-dconf write "$dconfdir/:$id"/use-system-font "false"
+dconf write "${dconfdir}/:${id}"/background-color "'rgb(0,0,0)'"
+dconf write "${dconfdir}/:${id}"/foreground-color "'rgb(0,255,0)'"
+dconf write "${dconfdir}/:${id}"/use-theme-colors "false"
+dconf write "${dconfdir}/:${id}"/default-size-columns "100"
+dconf write "${dconfdir}/:${id}"/default-size-rows "28"
+dconf write "${dconfdir}/:${id}"/font "'Monospace 14'"
+dconf write "${dconfdir}/:${id}"/use-system-font "false"
 
 
 ######################################
 ### Create profile - "Custom_Root"
 id=$(create_new_profile Custom_Root)
 # Preferences:
-dconf write "$dconfdir/:$id"/background-color "'rgb(9,9,9)'"
-dconf write "$dconfdir/:$id"/foreground-color "'rgb(213,0,0)'"
-dconf write "$dconfdir/:$id"/use-theme-colors "false"
-dconf write "$dconfdir/:$id"/default-size-columns "100"
-dconf write "$dconfdir/:$id"/default-size-rows "28"
-dconf write "$dconfdir/:$id"/font "'Monospace 14'"
-dconf write "$dconfdir/:$id"/use-system-font "false"
+dconf write "${dconfdir}/:${id}"/background-color "'rgb(9,9,9)'"
+dconf write "${dconfdir}/:${id}"/foreground-color "'rgb(213,0,0)'"
+dconf write "${dconfdir}/:${id}"/use-theme-colors "false"
+dconf write "${dconfdir}/:${id}"/default-size-columns "100"
+dconf write "${dconfdir}/:${id}"/default-size-rows "28"
+dconf write "${dconfdir}/:${id}"/font "'Monospace 14'"
+dconf write "${dconfdir}/:${id}"/use-system-font "false"
 
 ################################################################################
 ################################################################################
