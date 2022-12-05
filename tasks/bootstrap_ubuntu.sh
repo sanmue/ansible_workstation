d95189da1f1c1706f32dece66ffe765abf96ee93
Sandro <sandro@universalaccount.de>
2022-12-05 16:06:48 +0100

    bootstrap_ubuntu.sh: Update - vorhandenes Repo löschen und neu clonen

--- tasks/bootstrap_ubuntu.sh
+++ tasks/bootstrap_ubuntu.sh
@@ -54,17 +54,19 @@ echo ""
 echo "Erstelle neues Verzeichnis '${repodir}' und Unterverzeichnis '${playbookdir}' im Home-Verzeichnis von '${userid}' ..."
 if [ -d "/home/${userid}/${repodir}/${playbookdir}" ]; then
     echo "Verzeichnis '${playbookdir}' existiert bereits."
-    #sudo rm -r "/home/${userid}/${repodir}/${playbookdir}" # && mkdir -p "/home/${userid}/${repodir}/${playbookdir}"
-    cd "/home/${userid}/${repodir}/${playbookdir}"
-    git pull origin
-    cd
+    sudo rm -r "/home/${userid}/${repodir}/${playbookdir}" # && mkdir -p "/home/${userid}/${repodir}/${playbookdir}"
+    #cd "/home/${userid}/${repodir}/${playbookdir}"
+    #git pull origin
+    #cd
 else
     mkdir -p "/home/${userid}/${repodir}/${playbookdir}"
-
-    echo ""
-    echo "Clone github-Repo des Ansible-Playbooks ins Verzeichnis '${playbookdir}' ..."
-    git clone ${githubOnlineRepo} "/home/${userid}/${repodir}/${playbookdir}"
 fi
+
+
+echo ""
+echo "Clone github-Repo des Ansible-Playbooks ins Verzeichnis '${playbookdir}' ..."
+git clone ${githubOnlineRepo} "/home/${userid}/${repodir}/${playbookdir}"
+
 
 echo ""
 read -rp "Soll TEST des Ansible-Playbooks durchgeführt werden (j/n)?: " testplay
