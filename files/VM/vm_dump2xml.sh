26190d3eb1c7494341a391beefd63238c83101f7
Sandro <sandro@universalaccount.de>
2022-12-27 17:28:14 +0100

    create virtual networks

--- files/VM/vm_dump2xml.sh
+++ files/VM/vm_dump2xml.sh
@@ -4,9 +4,9 @@ exportPath=$(pwd)
 
 # net-dumpxml networks:
 virsh net-list --name | xargs -I % sh -c "virsh net-dumpxml % > ${exportPath}/netdump_%.xml"
-if [ -f "${exportPath}/netdump_default.xml" ]; then
-	rm "${exportPath}/netdump_default.xml"
-fi
+#if [ -f "${exportPath}/netdump_default.xml" ]; then
+#	rm "${exportPath}/netdump_default.xml"
+#fi
 
 # dumpxml virtual machines:
 virsh list --all --name | xargs -I % sh -c "virsh dumpxml % > ${exportPath}/dump_%.xml"
