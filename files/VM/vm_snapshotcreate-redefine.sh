056e60cefc893438fa0e3d179ba416b1f0d84857
Sandro <sandro@universalaccount.de>
2022-12-28 23:45:58 +0100

    Update: VM redefine

--- files/VM/vm_snapshotcreate-redefine.sh
+++ files/VM/vm_snapshotcreate-redefine.sh
@@ -8,29 +8,30 @@
 snapshotfileList=$(ls snapshotdump_*)
 #echo "${snapshotfileList}"
 
+echo -e "\nredefine snapshots:"
 for snapshotfile in ${snapshotfileList}; do
 	#echo "- snapshotfile: ${snapshotfile}"
 	domain=$(echo "${snapshotfile}" | cut -d _ -f 2)
-	#echo "- domain: ${domain}"
-	#snapshot=$(echo "${snapshotfile}" | cut -d _ -f 3 | cut -d . -f 1)
-	#echo "- snapshot: ${snapshot}"
+	echo "- domain: ${domain}"
 
 	# snapshot-create --redefine
 	virsh snapshot-create "${domain}" --xmlfile "${snapshotfile}" --redefine
 done
 
 
-#########################
-### set current snapshots
-#########################
+########################
+### set current snapshot
+########################
 snapshotcurrentfileList=$(ls snapshotcurrent_*)
 #echo "${snapshotcurrentfileList}"
 
+echo -e "\nset current snapshot:"
 for snapshotcurrentfile in ${snapshotcurrentfileList}; do
 	#echo "- snapshotcurrentfile: ${snapshotcurrentfile}"
-	#domain=$(echo "${snapshotcurrentfile}" | cut -d _ -f 2 | cut -d . -f 1)   # Problem bei domain 'ubuntu22.04' wegen 'xxx22.04.txt"
+	#domain=$(echo "${snapshotcurrentfile}" | cut -d _ -f 2 | cut -d . -f 1)   
+															# Problem bei domain z.B. 'ubuntu22.04' wegen '." in Domain-Name
 	domain=$(echo "${snapshotcurrentfile}" | cut -d _ -f 2 | awk '{print substr($0,1,length-4)}')   # letzte 4 Zeichen abschneiden (.txt)
-	echo "- domain: ${domain}"
+	echo -e "\n- domain: ${domain}"
 	snapshotcurrent=$(cat "${snapshotcurrentfile}")
 	#echo "- snapshotcurrent: ${snapshotcurrent}"
 
