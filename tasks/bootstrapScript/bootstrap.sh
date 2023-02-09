64c860ac16ca87738a102b998ebdf611628b674d
sandro <sandro@universalaccount.de>
2023-02-10 00:18:02 +0100

    bootstrap: noconfirm sysupdate nach update mirrorlist

--- tasks/bootstrapScript/bootstrap.sh
+++ tasks/bootstrapScript/bootstrap.sh
@@ -70,7 +70,7 @@ case ${os} in
             touch "/home/${userid}/.bootstrapMirrorPool"
 
             echo -e "\nReset custom mirror list + customize mirror pool + full refresh of the package database and update all packages on the system..."
-            sudo pacman-mirrors -c all && sudo pacman-mirrors --country Germany,France,Austria,Switzerland,Netherlands && sudo pacman -Syyu
+            sudo pacman-mirrors -c all && sudo pacman-mirrors --country Germany,France,Austria,Switzerland,Netherlands && sudo pacman -Syyu --noconfirm 
         fi
 
         echo -e "\nInstallation initial benoetigte Software (git, ansible, openssh, ufw)..."
