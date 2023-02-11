dbdec6cc9f1f3399c7822b8ea329e5ddccc73e0b
sandro <sandro@universalaccount.de>
2023-02-11 09:31:34 +0100

    VM+fix: Reorga Dateien, fix für UEFI-Secureboot (OVMF) unter Manjaro bei VMs aus Ubuntu

---
+++ tasks/fix/config_all-VM-QemuKvm-create_OVMF_Manjaro_fix.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+### für Manjaro/Archlinux: Bei Neuerstellung QEMU/KVM-VMs aus Ubuntu
+### Stand: 02/2023, VMs aus Ubuntu 22.04
+# - unter Ubuntu zeigen die Pfade für UEFI/Secureboot auf einen anderen Pfad
+# - erstelle daher Symlinks für entsprechende Pfade bei Manjaro
+
+ovmfPath="/usr/share/OVMF"
+ovmf64Path="${ovmfPath}/x64"
+edk264Path="/usr/share/edk2/x64"
+
+if [ -e "${ovmf64Path}" ]; then
+	ovmf64List=$(sudo ls "${ovmf64Path}")
+else
+	echo "Verzeichnis '${ovmf64Path}' existiert nicht."
+	echo "Symlinks können nicht angelegt werden. Breche ab."
+	exit 1
+fi
+
+if [ ! -e "${edk264Path}" ]; then
+	echo "Zielverzeichnis '${edk264Path}' für Symlinks nicht vorhanden. Breche ab."
+	exit 1
+fi
+
+for listItem in ${ovmf64List}; do
+	if [ ! -e "${ovmfPath}/${listItem}" ]; then
+		#echo "Erstelle Symlink für ${listItem}..."
+		sudo ln -s "${edk264Path}/${listItem}" "${ovmfPath}/${listItem}"
+	#else
+		#echo "'${ovmfPath}/${listItem}' bereits vorhanden"
+	fi	
+done
+
