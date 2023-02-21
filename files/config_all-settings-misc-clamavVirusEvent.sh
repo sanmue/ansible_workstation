caaae691f5af166cfb615958725d2e57ed3ce8ab
sandro <sandro@universalaccount.de>
2023-02-21 18:58:11 +0100

    config_all-settings-misc-clamavVirusEvent.sh: lokale Mail an USER bei Signaturfund

--- files/config_all-settings-misc-clamavVirusEvent.sh
+++ files/config_all-settings-misc-clamavVirusEvent.sh
@@ -3,9 +3,12 @@
 PATH=/usr/bin
 ALERT="Signature detected by clamav: '${CLAM_VIRUSEVENT_VIRUSNAME}' in '${CLAM_VIRUSEVENT_FILENAME}'"
 
-# Send an alert to all graphical users.
+# Send an alert to all graphical users:
 for ADDRESS in /run/user/*; do
     USERID=${ADDRESS#/run/user/}
     /usr/bin/sudo -u "#$USERID" DBUS_SESSION_BUS_ADDRESS="unix:path=$ADDRESS/bus" PATH=${PATH} \
     /usr/bin/notify-send -i dialog-warning "Virus found!" "$ALERT"
 done
+
+# Send local mail alert to ${USER}:
+echo "Signature detected by clamav: '${CLAM_VIRUSEVENT_VIRUSNAME}' in '${CLAM_VIRUSEVENT_FILENAME}'" | /usr/bin/mail -s "Signature detected in '${CLAM_VIRUSEVENT_FILENAME}'" "${USER}"
