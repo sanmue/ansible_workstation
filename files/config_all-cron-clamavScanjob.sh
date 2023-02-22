564036914739ea9221aab9933631016d20f611f4
sandro <sandro@universalaccount.de>
2023-02-22 17:06:11 +0100

    config_all-cron-clamavScanjob.sh: Reorga, Funktionen

--- files/config_all-cron-clamavScanjob.sh
+++ files/config_all-cron-clamavScanjob.sh
@@ -4,82 +4,81 @@
 ### Variablen
 #############
 PATH=/usr/bin
-arrUser=($(users))   # array of logged on users
 
 scanPath="/home/sandro"     # default-Wert
 if [ $# -gt 0 ]; then
     scanPath="${1}"         # Übergabe-Parameter an Script
 fi
 
-if [ ! -d "${scanPath}" ]; then
-    errMsgSubj="ClamAV (cronjob) - Fehler"
-    errMsg="${errMsgSubj}: Path '${scanPath}' nicht vorhanden. \nScript: '$0'"
-
-    for ADDRESS in /run/user/*; do
-        USERID=${ADDRESS#/run/user/}
-        /usr/bin/sudo -u "#$USERID" DBUS_SESSION_BUS_ADDRESS="unix:path=$ADDRESS/bus" PATH=${PATH} \
-        /usr/bin/notify-send -i dialog-warning "ClamAV" "${errMsg}"
-    done    
-
-    for user in ${arrUser}; do
-        echo -e "${errMsg}" | /usr/bin/mail -s "${errMsgSubj}" "${user}"
-    done
-
-    exit 1
-fi
-
 startMsgSubj="ClamAV (cronjob) - Scan started"
-startMsg="${startMsgSubj} for path '${scanPath}'. \nScript: '$0'"
+startMsg="${startMsgSubj} for path '${scanPath}'.\nScript: '$0'"
 finMsgSubj="ClamAV (cronjob) - Scan finished"
-finMsg="${finMsgSubj} for path '${scanPath}'. \nScript: '$0'"
+finMsg="${finMsgSubj} for path '${scanPath}'.\nScript: '$0'"
+errMsgSubj="ClamAV (cronjob) - Error"
+errMsg="${errMsgSubj}: Path '${scanPath}' does not exist.\nScript: '$0'"
 
 
 ##############
 ### Funktionen
 ##############
-# TODO
+function notify-allGuiUser() { 
+	# Param1: Notify Subject
+    # Param2: Notify Message
+
+    for ADDRESS in /run/user/*; do
+        USERID=${ADDRESS#/run/user/}
+        /usr/bin/sudo -u "#$USERID" DBUS_SESSION_BUS_ADDRESS="unix:path=$ADDRESS/bus" PATH=${PATH} \
+        /usr/bin/notify-send -i dialog-warning "${1}" "${2}"
+    done 
+}
+
+function mail-allLogonUser() { 
+	# Param1: Mail Subject
+    # Param2: Mail Message
+    arrUser=($(users))      # array of logged on users
+
+    for user in ${arrUser}; do
+        echo -e "${2}" | /usr/bin/mail -s "${1}" "${user}"
+    done
+}
 
 
 ########
 ### main
 ########
 
+# ### --------------
+# ### Check scanPath
+# ### --------------
+if [ ! -d "${scanPath}" ]; then
+    # --- Send an alert to all graphical users:
+    notify-allGuiUser "${errMsgSubj}" "${errMsg}"
+    # --- Send (local) mail alert to all logged on users:
+    mail-allLogonUser "${errMsgSubj}" "${errMsg}"
+
+    exit 1
+fi
+
 # ### --------------------------------
 # ### Send start notification to users
 # ### --------------------------------
 
 # --- Send an alert to all graphical users:
-for ADDRESS in /run/user/*; do
-    USERID=${ADDRESS#/run/user/}
-    /usr/bin/sudo -u "#$USERID" DBUS_SESSION_BUS_ADDRESS="unix:path=$ADDRESS/bus" PATH=${PATH} \
-    /usr/bin/notify-send -i dialog-warning "ClamAV" "${startMsg}"
-done
-
+notify-allGuiUser "${startMsgSubj}" "${startMsg}"
 # --- Send (local) mail alert to all logged on users:
-for user in ${arrUser}; do
-    echo -e "${startMsg}" | /usr/bin/mail -s "${startMsgSubj}" "${user}"
-done
-
+mail-allLogonUser "${startMsgSubj}" "${startMsg}"
 
 # ### -------
 # ### Scanjob
 # ### -------
-
 # /usr/bin/clamdscan --fdpass --multiscan --move="${scanPath}/.clam/quarantine" --log="${scanPath}/.clam/logs/$(date +\%Y\%m\%d)-weekly.log" "${scanPath}" 2>/dev/null 1>&2
-
 
 # ### --------------------------------
 # ### Send final notification to users
 # ### --------------------------------
 
 # --- Send an alert to all graphical users:
-for ADDRESS in /run/user/*; do
-    USERID=${ADDRESS#/run/user/}
-    /usr/bin/sudo -u "#$USERID" DBUS_SESSION_BUS_ADDRESS="unix:path=$ADDRESS/bus" PATH=${PATH} \
-    /usr/bin/notify-send -i dialog-warning "ClamAV" "${finMsg}"
-done
-
+notify-allGuiUser "${finMsgSubj}" "${finMsg}"
 # --- Send (local) mail alert to all logged on users:
-for user in ${arrUser}; do
-    echo -e "${finMsg}" | /usr/bin/mail -s "${finMsgSubj}" "${user}"
-done
+mail-allLogonUser "${finMsgSubj}" "${finMsg}"
+
