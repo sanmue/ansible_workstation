ef20746489b1b7b9424a21b167b06c9635a5990d
sandro <sandro@universalaccount.de>
2023-02-22 13:14:33 +0100

    config_all-settings-misc-clamavVirusEvent.sh: lokale Mail: bugfix array logged on users

--- files/config_all-settings-misc-clamavVirusEvent.sh
+++ files/config_all-settings-misc-clamavVirusEvent.sh
@@ -3,15 +3,25 @@
 PATH=/usr/bin
 ALERT="Signature detected by clamav: '${CLAM_VIRUSEVENT_VIRUSNAME}' in '${CLAM_VIRUSEVENT_FILENAME}'"
 
-# Send an alert to all graphical users:
+# ### Send an alert to all graphical users:
 for ADDRESS in /run/user/*; do
     USERID=${ADDRESS#/run/user/}
     /usr/bin/sudo -u "#$USERID" DBUS_SESSION_BUS_ADDRESS="unix:path=$ADDRESS/bus" PATH=${PATH} \
     /usr/bin/notify-send -i dialog-warning "Virus found!" "$ALERT"
 done
 
-# Send local mail alert to all logged on users:
-userList=$(who -s | cut -d " " -f 1)
-for user in ${userList}; do
+
+# ### Send (local) mail alert to all logged on users:
+# - https://www.shellcheck.net/wiki/SC2207
+# - https://www.golinuxcloud.com/bash-split-string-into-array-linux/
+# - IFS (Internal Field Separator)
+
+#arrUser=()
+#IFS=" " read -r -A arrUser <<< "$(users)"  # funktioniert bei zsh (Archlinux)
+#IFS=" " read -r -a arrUser <<< "$(users)"  # funktioniert bei bash (Ubuntu)
+arrUser=($(users))                          # funktioniert bei zsh (Archlinux) und bash (Ubuntu)
+
+for user in ${arrUser}; do
     echo "${ALERT}" | /usr/bin/mail -s "Signature detected in '${CLAM_VIRUSEVENT_FILENAME}'" "${user}"
 done
+
