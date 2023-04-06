a7fbb082c8cf0b5d8f74fde1aa42a80b6b566a0a
sandro <sandro@universalaccount.de>
2023-04-06 16:56:40 +0200

    restoreHome.sh hinzugefügt

---
+++ tasks/ScriptsExtern/restoreHome.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#set -x
+
+# ### rsync - zusätzliche Parameter:
+paramRsync='--dry-run'
+#paramRsync=''
+
+# ### Variablen / Prüfung:
+dest=${HOME}
+
+if [ $# -gt 0 ]; then   # wenn (mehr als 0) Übergabeparameter vorhanden
+	source=$1			# erster Parameter: Quellpfad
+	source=${source%/}	# '/' am Ende entfernen, wenn vorhanden
+else
+	echo "Parameter 1 für Quellpfad wurde nicht übergeben, Ende."
+	exit 1
+	#source='/run/media/sandro/Seagate8TB-crypt'
+fi
+
+# Prüfung Quellpfad:
+if [ -e "${source}" ]; then				# Prüfung, ob Quellpfad existiert
+	echo "Quellpfad ist: ${source}"
+else
+	echo "Parameter 1: Quellpfad '${source}' existiert nicht, Ende."
+	exit 1
+fi
+
+
+# ### ##########################
+# ### Restore home aus Sicherung
+echo -e "\n ======================================== "
+echo "Starte restore von '${source}' nach '${dest}'"
+logname="restoreHome_$(date +"%Y-%m-%d_%H%M%S").log"
+#rsync -aPhEv "${paramRsync}" "${source}/" "${dest}/" | tee "/tmp/${logname}"   # ### dry-run
+rsync -aPhEv "${source}/" "${dest}/" | tee "/tmp/${logname}"
+echo ' ======================================== '
