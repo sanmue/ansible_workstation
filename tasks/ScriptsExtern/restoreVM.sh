3ee6dc307ac884939a424ea058a2472ec7a109c8
sandro <sandro@universalaccount.de>
2023-04-06 18:06:01 +0200

    restoreVM.sh: hinzugefügt

---
+++ tasks/ScriptsExtern/restoreVM.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#set -x
+
+# ### rsync - zusätzliche Parameter:
+paramRsync='--dry-run'
+#paramRsync=''
+
+if [ $# -gt 1 ]; then   # wenn (mehr als 0) Übergabeparameter vorhanden
+	source=$1
+	source=${source%/}
+	dest=$2
+	dest=${dest%/}
+else
+	echo "Keine 2 Parameter (Quelle, Ziel), Ende."
+	exit 1
+fi
+
+# Prüfung Quell:
+if [ -e "${source}" ]; then             # Prüfung, ob Sicherungsziel existiert
+	echo "Quellpfad ist: ${dest}"
+else
+	echo "Parameter 1: Quellpfad '${source}' existiert nicht, Ende."
+	exit 1
+fi
+# Prüfung Zielpfad:
+if [ -e "${dest}" ]; then             # Prüfung, ob Sicherungsziel existiert
+	echo "Zielpfad ist: ${dest}"
+else
+	echo "Parameter 2: Zielpfad '${dest}' existiert nicht, Ende."
+	exit 1
+fi
+
+
+# ### ###############################
+# ### Restore VM
+
+read -rp "Start mit beliebiger Eingabe"
+
+# ### VM (sudo)
+echo "Starte restore von '${source}/' nach '${dest}/'"
+logname="restoreVM_$(date +"%Y-%m-%d_%H%M%S").log"
+# sudo rsync "${paramRsync}" -aPhEv --delete --force "${source}/" "${dest}/" | tee "/tmp/${logname}"   # ### dry-run
+sudo rsync -aPhEv --delete --force "${source}/" "${dest}/" | tee "/tmp/${logname}"
+echo " ======================================== "
