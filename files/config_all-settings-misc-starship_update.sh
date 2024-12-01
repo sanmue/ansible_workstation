bc4135d27de364aad5d281ff6fb8803decbab3cb
sanTmue <10733637-sanmue@users-noreply.gitlab.com>
2024-12-01 13:38:38 +0100

    update: starship update script (called via systemd)

--- files/config_all-settings-misc-starship_update.sh
+++ files/config_all-settings-misc-starship_update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#set -x   # enable debug mode
+# set -x   # enable debug mode
 
 ### ############################
 ### Update starship shell prompt
@@ -10,9 +10,21 @@
 ### -------------------
 ### Parameter/Variablen
 ### -------------------
-path="$HOME"     # default-Wert
-if [ $# -gt 0 ]; then
-    path="${1}"    # 1. Übergabe-Parameter an Script muss Pfad sein
+path="/root" # default value # download target for starship install script
+logFilePath="/var/log"
+logFileName="starship_update_systemd.log"
+logfile="${logFilePath}/${logFileName}"
+date=$(date)
+
+touch "${logfile}"
+
+if [ $# -gt 0 ]; then # if more than 0 parameter
+    if [ "$(ls "${1}")" ]; then
+        path="${1%/}" # 1. Übergabe-Parameter an Script muss Pfad sein; letztes Zeichen wird entfernt wenn '/'
+    else
+        echo "${date} - Path (Parameter 1) '${1}' not found, not updated." >> "${logfile}"
+        exit 1
+    fi
 fi
 
 ### ----
@@ -20,4 +32,6 @@ fi
 ### ----
 
 # --- Update:
-curl -sS https://starship.rs/install.sh > $path/starship_install.sh && chmod 755 $path/starship_install.sh && $path/starship_install.sh --yes && rm $path/starship_install.sh
+echo "${date} - Starship update started (systemd) - path '${path}'" >> "${logfile}"
+curl -sS https://starship.rs/install.sh > "$path/starship_install.sh" && chmod 755 "$path/starship_install.sh" && "$path/starship_install.sh" --yes # && rm "$path/starship_install.sh"
+echo "${date} - Starship update finished (systemd) - path '${path}'" >> "${logfile}"
