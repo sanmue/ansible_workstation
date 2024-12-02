7ab352091af2bafa4f7eba2a52f4cfa13aa4cad3
sanTmue <10733637-sanmue@users-noreply.gitlab.com>
2024-12-02 13:37:18 +0100

    update: pcloud mount script, alias created

--- files/config_all-settings-misc-rclone_pCloud-Mnt.sh
+++ files/config_all-settings-misc-rclone_pCloud-Mnt.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 if [ ! "$(ls "${HOME}/pCloud-Mnt")" ]; then
-    mkdir -p "${HOME}/pCloud-Mnt"
+    echo -e "\e[1;33mPfad '${HOME}/pCloud-Mnt' noch nicht vorhanden, wird erstellt...\e[0m"
+    mkdir -pv "${HOME}/pCloud-Mnt"
 fi
 
-echo "Mounting pcloud to '${HOME}/pCloud-Mnt'"
-echo "Leave terminal window/session open as long as mount is required."
-echo "To stop / unmount: press <CTRL>+<C> or close terminal window/session."
+echo -e "\nMounting pcloud to '${HOME}/pCloud-Mnt'"
+echo "Leave terminal window / session open as long as mount is required."
+echo -e "To stop / unmount: press <CTRL>+<C> or close terminal window / session.\n"
 
 rclone --vfs-cache-mode writes mount pcloud: "${HOME}/pCloud-Mnt"
