1c5128396fe04e3de77e6064e386071dcbdc38ae
sanTmue <10733637-sanmue@users-noreply.gitlab.com>
2025-06-15 14:25:40 +0200

    modified: added alias for pip install --user -r requurements...txt; modfied: script 'config_all-settings-misc-ansible_update-by-tag.sh', absolut path to repo replaced with find...

--- files/config_all-settings-misc-ansible_update-by-tag.sh
+++ files/config_all-settings-misc-ansible_update-by-tag.sh
@@ -2,23 +2,35 @@
 
 #set -x   # enable debug mode
 
-REPO_DIR="${HOME}/Downloads/ansible_workstation_repo"
+REPO_NAME="ansible_workstation"
+repo_path=$(find "${HOME}" -type d -name "${REPO_NAME}")
+
+# Check if repo_path empty (path to folder 'REPO_NAME' not found)
+if [ -z "${repo_path}" ]; then
+    echo -e "\e[31m- Folder '${REPO_NAME}' not found."
+
+    repo_path="${HOME}/.${REPO_NAME}_repo"
+    echo "- setting target repo_path to '${repo_path}'"
+
+    # Check if new path for REPR_DIR already exists (already cloned)
+    if [ ! -d "${repo_path}" ]; then # if repo dir does not yet exist
+        git clone https://gitlab.com/sanmue/ansible_workstation.git "${repo_path}"
+    else # if repo already exists (cloned) -> update
+        cd "${repo_path}" && git pull
+    fi
+else
+    # will not be executed, since repo will be cloned in if-statement above, if folder REPO_NAME not found
+    echo -e "- Folder '${REPO_NAME}' found at: \n${repo_path}"
+fi
 
 # Check number of parameters
 if [ $# -le 0 ] || [ $# -gt 1 ]; then # if no parameter or more than 1 parameter passed
-    echo -e "No parameter or more than 1 parameter passed.\nEnding script."
+    echo -e "\e[31m- No parameter or more than 1 parameter passed to the script.\e[0m"
     exit 1
 else
     tag="${1}"
-fi
-
-
-if [ ! -d "${REPO_DIR}" ]; then # if repo dir does not yet exist
-    git clone https://gitlab.com/sanmue/ansible_workstation.git "${REPO_DIR}"
-else # if repo already cloned -> update
-    cd "${REPO_DIR}" && git pull
+    echo "- Parameter passed to the script: '${tag}'"
 fi
 
 # sudo in advance, only if systemwide conf/cmd required
-sudo true && ansible-playbook "${REPO_DIR}/local.yml" -v -k --tags "${tag}"
-
+sudo true && ansible-playbook "${repo_path}/local.yml" -v -k --tags "${tag}"
